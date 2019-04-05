796ca35b8c9004655a9098ebabd721ae5be0e6ca
bonjih <37001472+bonjih@users.noreply.github.com>
2019-04-05 14:11:42 +1000

    changed to_datetime syntax to pandas 0.24.2

--- Fourier-Transform_pps_rx.py
+++ Fourier-Transform_pps_rx.py
@@ -9,11 +9,13 @@ df = pd.read_csv('data/test_pps_rx_1_month_epoch.csv', usecols=['Date','Utilisat
 
 df['Date'] = pd.to_datetime(df.Date, unit='s')
 df.set_index('Date',inplace=True)
-df_avg = df.dropna().groupby('Date').mean()
+df_sum = df.dropna().groupby('Date').sum()
 
-date = df_avg.index.to_datetime()
-df = (df_avg['Utilisation pps Rx'])
-# N = len(df)
+date = pd.to_datetime(df.index)
+
+df = (df_sum['Utilisation pps Rx'])
+
+N = len(df)
 
 fig, ax = plt.subplots(1, 1, figsize=(6, 3))
 df.plot(ax=ax, lw=.5)
