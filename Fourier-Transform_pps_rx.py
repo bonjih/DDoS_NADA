aa5ac8fc05ec4bb691ebf6e9032dabcb31393828
bonjih <37001472+bonjih@users.noreply.github.com>
2019-04-05 14:50:46 +1000

    added images

--- Fourier-Transform_pps_rx.py
+++ Fourier-Transform_pps_rx.py
@@ -26,21 +26,25 @@ fftfreq = sp.fftpack.fftfreq(len(pps_psd), 1 / 31) #original unit is a month
 
 # Find the peak frequency, only the positive frequencies
 i = np.where(fftfreq > 0)
-
 freqs = fftfreq[i]
 peak_freq = freqs[pps_psd [i].argmax()]
-print(peak_freq)
+
+# An inner plot to show the peak frequency
+axes = plt.axes([0.55, 0.3, 0.3, 0.5])
+plt.title('Peak frequency')
+plt.plot(freqs[:8], pps_psd[:8])
+plt.setp(axes, yticks=[])
+
 #lot the power spectral density
 fig, ax = plt.subplots(1, 1, figsize=(8, 4))
 ax.plot(fftfreq[i], 10 * np.log10(pps_psd[i]))
 ax.set_xlabel('Frequency (1/month)')
 ax.set_ylabel('PSD (dB)')
 
-#Remove all the high frequencies
+# Remove all the high frequencies and transform back from frequencies to signal
 high_freq_fft = pps_fft.copy()
 high_freq_fft[np.abs(fftfreq) > peak_freq] = 0
 filtered_sig = sp.fftpack.ifft(high_freq_fft)
-
 
 # filtered_sig = np.real(sp.fftpack.ifft(pps_fft_bis))
 fig, ax = plt.subplots(1, 1, figsize=(18, 6))
